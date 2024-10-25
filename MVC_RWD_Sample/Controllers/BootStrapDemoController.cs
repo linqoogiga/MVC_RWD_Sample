4776627310302c07c3df6ba0a7d68f435c3760a4
OMEGA3\johnlin <johnlin@brandedlifestyle.com>
2024-10-25 15:35:36 +0800

    Ver1

---
+++ MVC_RWD_Sample/Controllers/BootStrapDemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_RWD_Sample.Controllers
+{
+    public class BootStrapDemoController : Controller
+    {
+        // GET: BootStrapDemo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ch1_Container()
+        {
+            return View();
+        }
+
+        public ActionResult Ch2_FlexBox()
+        {
+            return View();
+        }        
+    }
+}
---
+++ MVC_RWD_Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_RWD_Sample.MvcApplication" Language="C#" %>
